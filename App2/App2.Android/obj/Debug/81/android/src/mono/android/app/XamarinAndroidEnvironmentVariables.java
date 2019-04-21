78cefacb812df148511fd113a601f0c2d532c680
Amrazyan <amrazyan@gmail.com>
2019-04-21 13:28:51 +0400

    grid layouts

--- App2/App2.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ App2/App2.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "74b731b0-2990-4b31-9c0c-007d05d823fc",
+		"XAMARIN_BUILD_ID", "8675402e-d5c6-4577-ae6a-472140c3ed2d",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
---
+++ App2/App2/Views/EntryPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Plugin.LocalNotifications;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Entry : ContentPage
+    {
+        public Entry()
+        {
+            InitializeComponent();
+            Title = "Ask your name";
+            Button1.Clicked += Button1_Clicked;
+            Button2.Clicked += OpenNewPage;
+        }
+
+        private async void OpenNewPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MyPage());
+        }
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            CrossLocalNotifications.Current.Show("Success Registere", $"{Email.Text } {Environment.NewLine} {Password.Text}",1);
+        }
+    }
+}
